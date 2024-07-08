6ac17bd26883ce27bab212b1011a5a04ed473ad6
joel martinez <joel.martinez@salesforce.com>
2024-07-08 09:25:00 -0700

    Basic first idea and implementation of what the storage cordinator should be further features to be implemented

---
+++ storage/storage_coord/storage_coord.py
@@ -0,0 +1,6 @@
+class StorageCoord:
+    """
+    This class should be able to coordinate the way that milvus uses persistent storage for log and index files
+    """
+    def __init__(self):
+        print("Storage Coord")
