5e0f56b7478916c706ca9e0bb0ff9737883bb420
joel martinez <joel.martinez@salesforce.com>
2024-07-07 23:10:31 -0700

    embedding_controller.py This class should wrap the functionality of when a controller is being used in a more efficient way meaning this will have a singleton pattern in which only one embedding controller will exists for which it will act when an embedding is being done in an efficent manner avoiding memory leakage

---
+++ models/embeddings/controller/embedding_controller.py
@@ -0,0 +1,10 @@
+"""
+This class should handle the functionality of the controller so that the Milvus framework can only call creation
+of embedding when needed from a given data input
+
+"""
+
+
+class EmbeddingController:
+    def __init__(self):
+        print("Initiallizing controller ")
